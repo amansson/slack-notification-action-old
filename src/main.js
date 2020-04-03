bb3d506dab50b9d9ca0fc3d89f098fc6e82fd629
andreas <andreas.mansson1@ingka.ikea.com>
2020-04-03 09:28:59 +0200

    change implementation to webhook instead of bot

--- src/main.js
+++ src/main.js
@@ -1,59 +1,18 @@
-  
-const { context } = require('@actions/github');
+import * as core from '@actions/core';
+import { IncomingWebhook } from '@slack/webhook';
 
-function buildSlackAttachments({ status, color, github }) {
-  const { payload, ref, workflow, eventName } = github.context;
-  const { owner, repo } = context.repo;
-  const event = eventName;
-  const branch = event === 'pull_request' ? payload.pull_request.head.ref : ref.replace('refs/heads/', '');
+async function run() {
+  try {
+    if (process.env.SLACK_WEBHOOK_URL === undefined) {
+      throw new Error('SLACK_WEBHOOK_URL is not set');
+    }
+    const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+    var payload = eval("payload = " + core.getInput('payload'));
 
-  const sha = event === 'pull_request' ? payload.pull_request.head.sha : github.context.sha;
-
-  const referenceLink =
-    event === 'pull_request'
-      ? {
-          title: 'Pull Request',
-          value: `<${payload.pull_request.html_url} | ${payload.pull_request.title}>`,
-          short: true,
-        }
-      : {
-          title: 'Branch',
-          value: `<https://github.com/${owner}/${repo}/commit/${sha} | ${branch}>`,
-          short: true,
-        };
-
-  return [
-    {
-      color,
-      fields: [
-        {
-          title: 'Action',
-          value: `<https://github.com/${owner}/${repo}/commit/${sha}/checks | ${workflow}>`,
-          short: true,
-        },
-        {
-          title: 'Status',
-          value: status,
-          short: true,
-        },
-        referenceLink,
-        {
-          title: 'Event',
-          value: event,
-          short: true,
-        },
-      ],
-      footer_icon: 'https://github.githubassets.com/favicon.ico',
-      footer: `<https://github.com/${owner}/${repo} | ${owner}/${repo}>`,
-      ts: Math.floor(Date.now() / 1000),
-    },
-  ];
+    await webhook.send(JSON.parse(JSON.stringify(payload)));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-module.exports.buildSlackAttachments = buildSlackAttachments;
-
-function formatChannelName(channel) {
-  return channel.replace(/[#@]/g, '');
-}
-
-module.exports.formatChannelName = formatChannelName;
+run();
